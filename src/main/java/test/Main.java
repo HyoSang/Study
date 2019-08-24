672abff3d8768f6a98d6e2613faba91ee67d6d00
gogumaguman <wjdekdud_22@naver.com>
2019-08-24 13:03:05 +0900

    다영1

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String args[]) {
+        SpringApplication.run(Main.class, args);
+
+    }
+}
